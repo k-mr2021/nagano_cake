5ed765351d8609b268b0cc0c48f0b9dd47b31bfa
k-mr2021 <143944891+k-mr2021@users.noreply.github.com>
2023-12-02 06:43:35 +0000

    [Add] customer/配送先編集作成

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -8,16 +8,27 @@ class Public::AddressesController < ApplicationController
   
   def create
     @address = Address.new(address_params)
+    # Customer must existエラー/current_customer.idを指定し顧客と関連付ける
     @address.customer_id = current_customer.id
-    @address.save
-    # 一覧へリダイレクト
-    redirect_to addresses_path
+    if @address.save
+      # 一覧へリダイレクト
+      redirect_to addresses_path
+    else
+      rendr :index
+    end
   end
   
   def edit
-    
+    @address_found = Address.find(params[:id])
   end
   
+  def update
+    @address_found = Address.find(params[:id])
+    @address_found.update(address_params)
+    # 一覧へリダイレクト
+    redirect_to addresses_path
+  end
+
   def destroy
     address_found = Address.find(params[:id])
     address_found.destroy
@@ -32,6 +43,9 @@ class Public::AddressesController < ApplicationController
   end
   
 end
+
+
+
 
 
 
