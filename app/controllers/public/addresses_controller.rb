c9e0db56f21a2da726a8434c96bff5c53aaa0072
k-mr2021 <143944891+k-mr2021@users.noreply.github.com>
2023-12-02 06:25:43 +0000

    [Add]customer/配送先一覧作成

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -2,17 +2,28 @@ class Public::AddressesController < ApplicationController
   before_action :authenticate_customer!
   
   def index
-    @addresses = Address.all
     @address = Address.new
+    @addresses = Address.all
   end
   
   def create
     @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
     @address.save
     # 一覧へリダイレクト
     redirect_to addresses_path
   end
   
+  def edit
+    
+  end
+  
+  def destroy
+    address_found = Address.find(params[:id])
+    address_found.destroy
+    # 一覧へリダイレクト
+    redirect_to addresses_path
+  end
   
   private
   
@@ -21,4 +32,10 @@ class Public::AddressesController < ApplicationController
   end
   
 end
+
+
+
+
+
+
 
