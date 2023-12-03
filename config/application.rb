36f5aa02a8639df31c67bc8b64893a102bd97698
k-mr2021 <143944891+k-mr2021@users.noreply.github.com>
2023-12-03 12:59:14 +0000

    [Add] enum日本語 化

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module NaganoCake
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.i18n.default_locale = :ja
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -20,3 +20,4 @@ module NaganoCake
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
+
