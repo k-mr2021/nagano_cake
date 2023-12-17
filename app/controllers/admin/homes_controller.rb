4466e30f969107d9e44d637081ad3773240fb9da
k-mr2021 <143944891+k-mr2021@users.noreply.github.com>
2023-12-17 16:44:48 +0000

    adminサイド/レイアウト調整

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -3,9 +3,10 @@ class Admin::HomesController < ApplicationController
   
   def top
     @orders = Order.all.page(params[:page]).per(10)
+    # 注文個数を表示するため
+    @total_amount = 0
     # `last_name`と`first_name`を組み合わせてフルネームを生成し、そのフルネームを要素とする新しい配列を作成
     # @full_names = @orders.map { |customer| "#{customer.last_name} #{customer.first_name}" }
-    @total_amount = 0
   end
   
 end
