98cc1d4930063c735819416363e7b9274306aff1
k-mr2021 <143944891+k-mr2021@users.noreply.github.com>
2023-12-24 00:57:30 +0000

    [Add] 退会確認アラート追加

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -27,7 +27,7 @@ class Public::SessionsController < Devise::SessionsController
   # end
   
   def after_sign_in_path_for(resource)
-    customer_path(current_customer)
+    root_path
   end
   
   def after_sign_out_path_for(resource)
@@ -56,6 +56,7 @@ class Public::SessionsController < Devise::SessionsController
   end
   
 end
+
 
 
 
